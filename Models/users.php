3d36ce7b6391b123e8b7fdeb2d31e1494ff619e1
junep0403 <takejun1225@yahoo.co.jp>
2022-05-31 21:23:36 +0900

    ホーム機能

--- Models/users.php
+++ Models/users.php
@@ -93,7 +93,7 @@ function createUser(array $data)
 
     // パスワードチェック、不一致の場合->return
     if (!password_verify($password, $user['password'])) {
-        $mysqli->close;
+        $mysqli->close();
         return false;
     }
 
